16d11c71e161600dae44627ad7d26d4d4b20a3b2
Sam Wallan <44255917+swallan@users.noreply.github.com>
2020-08-18 02:27:00 -0700

    TST: add tests for gumbel_r.fit

--- scipy/stats/tests/test_distributions.py
+++ scipy/stats/tests/test_distributions.py
@@ -29,6 +29,7 @@ import scipy.stats.distributions
 from scipy.special import xlogy
 from .test_continuous_basic import distcont
 from scipy.stats._continuous_distns import FitDataError
+from scipy.optimize import root
 
 # python -OO strips docstrings
 DOCSTRINGS_STRIPPED = sys.flags.optimize > 1
@@ -894,6 +895,44 @@ class TestLogser(object):
         #   >>> float(-p / ((1 - p)*mpmath.log(1 - p)))
         #   1.000000005
         assert_allclose(m, 1.000000005)
+
+
+class TestGumbel_r(object):
+    def setup_method(self):
+        np.random.seed(1234)
+
+    @pytest.mark.parametrize("loc_rvs,scale_rvs", [np.random.rand(2)])
+    def test_fit(self, loc_rvs, scale_rvs):
+        data = stats.gumbel_r.rvs(size=100, loc=loc_rvs, scale=scale_rvs)
+
+        # test that result of fit method is the same as optimization
+        def func(vals, data):
+            a, b = vals
+            ndata = len(data)
+            x1 = data.mean() - ((np.sum(data * np.exp(- data / b))) /
+                                np.sum(np.exp(- data / b))) - b
+            x2 = - b * np.log(np.sum(np.exp(-data / b)) / ndata) - a
+            return x1, x2
+
+        expected_solution = root(func, stats.gumbel_r._fitstart(data), args=(
+            data,)).x
+        fit_method = stats.gumbel_r.fit(data)
+
+        # other than computational variances, the fit method and the solution
+        # to this system of equations are equal
+        assert_allclose(fit_method, expected_solution, atol=1e-30)
+
+    @pytest.mark.parametrize("loc_rvs,scale_rvs", [np.random.rand(2)])
+    def test_fit_comp_optimizer(self, loc_rvs, scale_rvs):
+        data = stats.gumbel_r.rvs(size=100, loc=loc_rvs, scale=scale_rvs)
+
+        # obtain objective function to compare results of the fit methods
+        args = [data, (stats.gumbel_r._fitstart(data),)]
+        func = stats.gumbel_r._reduce_func(args, {})[1]
+
+        # test that gumbel_r fit method is better than super method
+        _assert_lessthan_loglike(stats.gumbel_r, data, func)
+
 
 
 class TestPareto(object):
